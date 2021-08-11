3bc8e735e29d353a66fbf8ac40fc88cb74276549
ClydeRM <clyde.official@icloud.com>
2021-08-11 22:04:52 +0800

    change query to subscription install websocket

--- clinet/src/Chat.jsx
+++ clinet/src/Chat.jsx
@@ -3,26 +3,35 @@ import {
     ApolloClient,
     InMemoryCache,
     ApolloProvider,
-    useQuery,
+    useSubscription,
     useMutation,
     gql
 } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
 import {
     Container,
     Row,
     Col,
     FormInput,
     Button
-} from 'shards-react'
+} from 'shards-react';
 
+
+const link = new WebSocketLink({
+    uri: 'ws://localhost:4000/',
+    options: {
+        reconnect: true
+    }
+});
 
 const client = new ApolloClient({
+    link,
     uri: 'http://localhost:4000/', // change link to graphql server
     cache: new InMemoryCache()
 });
 
 const GET_MESSAGES = gql`
-    query {
+    subscription {
         messages {
             id
             content
@@ -38,9 +47,7 @@ const POST_MESSAGE = gql`
 `;
 
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES, {
-        pollInterval: 500,
-    });
+    const { data } = useSubscription(GET_MESSAGES);
     if (!data) {
         return null;
     }
