380945746a3156c4e04cd589279613a850b1724d
ClydeRM <clyde.official@icloud.com>
2021-08-11 21:51:29 +0800

    add Subscription and channel to alert server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer, PubSub } = require('graphql-yoga')
 
 const messages = [];
 
@@ -18,7 +18,15 @@ const typeDefs = `
     type Mutation {
         postMessage(user: String!, content: String!): ID!
     }
+
+    type Subscription {
+        messages: [Message!]
+    }
 `;
+
+// Who is on server now and use channal
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
 
 // get and post message
 // Query.messages return an array
@@ -27,20 +35,33 @@ const resolvers = {
         messages: () => messages,
     },
     Mutation: {
-        postMessage: (parent, {user, content}) => {
+        postMessage: (parent, { user, content }) => {
             const id = messages.length;
             messages.push({
                 id,
                 user,
                 content
             });
+            subscribers.forEach((fn) => fn());
             return id;
+        }
+    },
+    Subscription: {  // alert server online has a user send a message
+        messages: {
+            subscribe: (parent, args, { pubsub }) => {
+                const channel = Math.random().toString(36).slice(2, 15); // channel id
+                onMessagesUpdates(() => pubsub.publish(channel, { messages })); // someone pull message
+                setTimeout(() => pubsub.publish(channel, { messages }), 0); // async to update
+                return pubsub.asyncIterator(channel);
+            }
         }
     }
 };
 
+const pubsub = new PubSub();
+
 // server use typeDefs
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
 server.start(({ port }) => {
     console.log(`Starting server on http://localhost:${port}/`);
