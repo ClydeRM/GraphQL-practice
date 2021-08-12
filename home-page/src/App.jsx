6376bdf77c57c07664fd5af26435587723f66e87
ClydeRM <clyde.official@icloud.com>
2021-08-12 14:20:51 +0800

    add chat app from remote

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Container } from 'shards-react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+import Chat from 'chat/Chat';
+
+const App = () => (
+    <Container>
+        <p>21342341</p>
+        <h1>Chat!</h1>
+        <Chat />
+        <p>12342134123</p>
+    </Container>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
